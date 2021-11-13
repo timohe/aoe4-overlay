631c2896993c23bc577c337f47366a7f821c6b09
timohe <t.hegnauer@gmail.com>
2021-11-13 12:38:31 +0100

    formatting

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -43,63 +43,65 @@ export class HomeComponent implements OnInit {
 				const imageBuffer = Buffer.from(this.screenshot);
 				sharp(imageBuffer)
 					.resize(200)
-					.toFile('/Users/timo/Desktop/picture.png', (err, info) => { console.log(`This is the data`)});
-	});
-}
-
-
-saveScreenshotsAndRecognize() {
-	desktopCapturer.getSources({
-		types: ['screen'], thumbnailSize: {
-			width: 2560,
-			height: 1440,
-		}
-	})
-		.then(sources => {
-			this.screenshot = sources[0].thumbnail.toPNG(); // The image to display the screenshot
-			const imageBuffer = Buffer.from(this.screenshot);
-			Tesseract.recognize(
-				imageBuffer,
-				'eng',
-				{ logger: m => console.log(m) }
-			).then(({ data: { text } }) => {
-				console.log(text);
-				// this.playerName = text;
-				// this.ocrResult = text;
-				// this.ocrResult = 'solaire';
-				// this.getPlayerStats(this.ocrResult);
+					.toFile('/Users/timo/Desktop/picture.png', (err, info) => {
+						console.log(`This is the data`);
+					});
 			});
+	}
+
+
+	saveScreenshotsAndRecognize() {
+		desktopCapturer.getSources({
+			types: ['screen'], thumbnailSize: {
+				width: 2560,
+				height: 1440,
+			}
+		})
+			.then(sources => {
+				this.screenshot = sources[0].thumbnail.toPNG(); // The image to display the screenshot
+				const imageBuffer = Buffer.from(this.screenshot);
+				Tesseract.recognize(
+					imageBuffer,
+					'eng',
+					{ logger: m => console.log(m) }
+				).then(({ data: { text } }) => {
+					console.log(text);
+					// this.playerName = text;
+					// this.ocrResult = text;
+					// this.ocrResult = 'solaire';
+					// this.getPlayerStats(this.ocrResult);
+				});
+			});
+	}
+	// recognizeImage() {
+	// 	fs.readFile('/Users/timo/Desktop/picture.png', function(err, data) {
+	// 		if (err) {throw err;};
+	// 	});
+	// }
+
+	closeApp() {
+		const win = remote.getCurrentWindow();
+		win.close();
+	}
+
+
+	getPlayerStats(playerName: string) {
+		this.apiGetPlayerStats(playerName).subscribe((response) => {
+			console.log(`This is the data`);
+			this.elo = response.items[0].elo;
+			this.playerName = response.items[0].userName;
 		});
-}
-// recognizeImage() {
-// 	fs.readFile('/Users/timo/Desktop/picture.png', function(err, data) {
-// 		if (err) {throw err;};
-// 	});
-// }
+	}
 
-closeApp() {
-	const win = remote.getCurrentWindow();
-	win.close();
-}
-
-
-getPlayerStats(playerName: string) {
-	this.apiGetPlayerStats(playerName).subscribe((response) => {
-		console.log(`This is the data`);
-		this.elo = response.items[0].elo;
-		this.playerName = response.items[0].userName;
-	});
-}
-
-apiGetPlayerStats(playerName) {
-	return this.httpClient.post<PlayerApiResponse>(`https://api.ageofempires.com/api/ageiv/Leaderboard`, {
-		region: '7',
-		versus: 'players',
-		matchType: 'unranked',
-		teamSize: '3v3',
-		searchPlayer: playerName,
-		page: 1,
-		count: 100
-	}).pipe();
-}
+	apiGetPlayerStats(playerName) {
+		return this.httpClient.post<PlayerApiResponse>(`https://api.ageofempires.com/api/ageiv/Leaderboard`, {
+			region: '7',
+			versus: 'players',
+			matchType: 'unranked',
+			teamSize: '3v3',
+			searchPlayer: playerName,
+			page: 1,
+			count: 100
+		}).pipe();
+	}
 }
